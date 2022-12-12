b09bb924a02c060dba13675971281276de94704a
Hannah <hannah.lablab15@gmail.com>
2022-12-12 10:51:50 +0800

    Created BMI Calculator

---
+++ bmi_calculator.js
@@ -0,0 +1,26 @@
+function bmiCalculator () {
+
+var h = 182;
+var w = 62;
+
+let bmi = (w/((h * h)/10000)).toFixed(2)
+result = bmi;
+
+if (bmi < 18.5) {
+    console.log(" You are underweight");
+}
+else if (bmi >= 18.5 && bmi <= 24.9) {
+    console.log("Normal Weight");
+}
+else if (bmi >= 25 && bmi <= 29.9) {
+    console.log("Overweight");
+}
+else  {
+    console.log("Obese");
+}
+console.log("Height entered : " + h + "cm");
+console.log("Weight entered : " + w + "kg");
+}
+console.log("Your result is: " )
+bmiCalculator();
+ 
