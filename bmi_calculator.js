984216d3aa9bc51379bb5dbce8036870ccd0bd56
Hannah <hannah.lablab15@gmail.com>
2022-12-13 11:41:35 +0800

    Manual Testing for BMI Calculator

--- bmi_calculator.js
+++ bmi_calculator.js
@@ -1,26 +1,31 @@
-function bmiCalculator () {
+function bmiCalculator (h,w) {
 
-var h = 182;
-var w = 60;
+//  var h = 182;
+//  var w = 60;
 
 let bmi = (w/((h * h)/10000)).toFixed(2)
 result = bmi;
 
 if (bmi < 18.5) {
-    console.log(" You are underweight");
+    console.log("sYou are underweight");
 }
 else if (bmi >= 18.5 && bmi <= 24.9) {
-    console.log("Normal Weight");
+    console.log("You are Normal Weight");
 }
 else if (bmi >= 25 && bmi <= 29.9) {
-    console.log("Overweight");
+    console.log("You are Overweight");
 }
 else  {
-    console.log("Obese");
+    console.log("You are Obese");
 }
 console.log("Height entered : " + h + "cm");
 console.log("Weight entered : " + w + "kg");
 }
-console.log("Your result is: " )
-bmiCalculator();
+bmiCalculator(182,60); //correct
+bmiCalculator(182,100); //correct
+console.log("BMI Result : " + result);
+
+
+//bmi result is 18.5 below which is equal to underweight
+//bmi result 30.0 and Above is equalt to Obese
 
